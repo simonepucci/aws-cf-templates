21142fca2236c8a6f868306d1406b68938382d11
Simone Pucci <spucci@kiwi.byte-code.lan>
2018-05-31 14:00:05 +0200

    Fix output in cluster helper script

--- FARGATEMANAGER/clusters-helper.sh
+++ FARGATEMANAGER/clusters-helper.sh
@@ -14,23 +14,16 @@ function colecho(){
         [ "$1" == "0" ] && $SETCOLOR_SUCCESS;
         [ "$1" == "1" ] && $SETCOLOR_FAILURE;
         [ "$1" == "2" ] && $SETCOLOR_WARNING;
-        [ "$2" == "" ] || echo "$2";
+        [ "$2" == "" ] || echo -en "$2";
         $SETCOLOR_NORMAL;
 }
 
 aws ecs list-clusters --output text | awk '{print $2}' | cut -d '/' -f 2 | while read line;
 do
     BIGGER=$(aws ssm get-parameters-by-path --max-items 10000 --path /${line}/ --recursive --region eu-west-1 --output text | grep albvalue | awk {'print $4'} | sort -n | tail -n 1);
-    [ ${BIGGER} -eq ${BIGGER} 2>/dev/null ] || BIGGER="Not Foud!"
     echo -n "Last used albvalue for Cluster: ";
     colecho "0" "${line}";
-    echo -n "                                ";
-    colecho "1" "${BIGGER}";
+    echo -n " -> ";
+    colecho "1" "${BIGGER}\n";
 done
-
-
-
-
-
-	    
 
