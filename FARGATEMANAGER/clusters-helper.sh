044955fe4a313caa41437278353f2615a0c22867
Simone Pucci <spucci@kiwi.byte-code.lan>
2018-05-31 13:53:04 +0200

    Add cluster helper script

---
+++ FARGATEMANAGER/clusters-helper.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+#
+
+
+# Basic Utility Function
+# Write colored output recieve "colournumber" "message"
+function colecho(){
+        SETCOLOR_SUCCESS="echo -en \\033[1;32m";
+        SETCOLOR_NORMAL="echo -en \\033[0;39m";
+        SETCOLOR_FAILURE="echo -en \\033[1;31m";
+        SETCOLOR_WARNING="echo -en \\033[1;33m";
+        [ "$1" == "" ] && $SETCOLOR_NORMAL;
+        [ "$1" == "0" ] && $SETCOLOR_SUCCESS;
+        [ "$1" == "1" ] && $SETCOLOR_FAILURE;
+        [ "$1" == "2" ] && $SETCOLOR_WARNING;
+        [ "$2" == "" ] || echo "$2";
+        $SETCOLOR_NORMAL;
+}
+
+aws ecs list-clusters --output text | awk '{print $2}' | cut -d '/' -f 2 | while read line;
+do
+    BIGGER=$(aws ssm get-parameters-by-path --max-items 10000 --path /${line}/ --recursive --region eu-west-1 --output text | grep albvalue | awk {'print $4'} | sort -n | tail -n 1);
+    [ ${BIGGER} -eq ${BIGGER} 2>/dev/null ] || BIGGER="Not Foud!"
+    echo -n "Last used albvalue for Cluster: ";
+    colecho "0" "${line}";
+    echo -n "                                ";
+    colecho "1" "${BIGGER}";
+done
+
+
+
+
+
+	    
+
